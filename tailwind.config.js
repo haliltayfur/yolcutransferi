72082a7ab1571bbd42b6c221eb4b27bc4f1c5a0e
haliltayfur <byhaliltayfur@hotmail.com>
2025-05-26 23:22:50 +0300

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,16 @@
-
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gold: "#C2A46F"
+      }
+    }
   },
   plugins: [],
 }
