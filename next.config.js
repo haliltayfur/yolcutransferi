0f4bd0d8e01cf3022fdd359839744748461bdd27
haliltayfur <byhaliltayfur@hotmail.com>
2025-05-27 12:17:27 +0300

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,1 +1,9 @@
-module.exports = { reactStrictMode: true };
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  i18n: {
+    locales: ['tr', 'en', 'ru'],
+    defaultLocale: 'tr',
+  },
+}
+
+module.exports = nextConfig;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,15 +2,20 @@
 module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}"
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
       colors: {
-        gold: "#C2A46F"
+        gold: "#d4af37",
+        navy: "#1a2238",
+        gray: "#F5F5F5"
+      },
+      fontFamily: {
+        serif: ["DM Serif Display", "serif"],
+        sans: ["Montserrat", "sans-serif"]
       }
-    }
+    },
   },
   plugins: [],
 }
