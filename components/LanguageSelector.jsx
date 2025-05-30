e3b216d42d9f8423087b3e24d8e12f1670379fca
haliltayfur <byhaliltayfur@hotmail.com>
2025-05-30 19:21:04 +0300

    Create LanguageSelector.jsx

---
+++ components/LanguageSelector.jsx
@@ -0,0 +1,9 @@
+export default function LanguageSelector() {
+  return (
+    <div className="flex gap-2 items-center">
+      <button title="Türkçe"><span className="text-xl">🇹🇷</span></button>
+      <button title="English"><span className="text-xl">🇬🇧</span></button>
+      <button title="العربية"><span className="text-xl">🇸🇦</span></button>
+    </div>
+  );
+}
