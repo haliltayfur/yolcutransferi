d1b5209ba9756cceae872f81fd8b29987e30f17e
haliltayfur <byhaliltayfur@hotmail.com>
2025-06-01 14:18:17 +0300

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -8,5 +8,7 @@ export default createMiddleware({
 });
 
 export const config = {
-  matcher: ['/((?!api|_next|favicon.ico|robots.txt|.*\\..*).*)'],
+  matcher: [
+    '/((?!api|_next/static|_next/image|favicon.ico|robots.txt|.*\\..*).*)',
+  ],
 };
