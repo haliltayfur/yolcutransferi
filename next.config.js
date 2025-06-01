6e24d3d31318d7a4767e7daaddc1071fb77525e1
haliltayfur <byhaliltayfur@hotmail.com>
2025-06-01 14:44:03 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,17 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-i18n: {
-  locales: ['tr', 'en', 'ar'],
-  defaultLocale: 'tr',
-  localeDetection: false // ← doğru!
-},
-  // Diğer ayarların...
-};
-
-module.exports = nextConfig;
-module.exports = {
   i18n: {
     locales: ['tr', 'en', 'ar'],
     defaultLocale: 'tr',
+    localeDetection: false, // Tarayıcı dili algılamasını kapatır (önerilir)
   },
+  reactStrictMode: true, // (opsiyonel ama tavsiye edilir)
 };
+
+module.exports = nextConfig;
