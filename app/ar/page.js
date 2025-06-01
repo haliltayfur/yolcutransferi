bc40435194586bc84451be49abf07fcd8a6e2462
haliltayfur <byhaliltayfur@hotmail.com>
2025-06-01 19:22:49 +0300

    Delete app/ar directory

--- app/ar/page.js
+++
@@ -1,7 +0,0 @@
-export default function ArHome() {
-  return (
-    <main className="min-h-screen bg-black text-white flex items-center justify-center">
-      <h1 className="text-3xl font-bold">مرحبًا بكم في YolcuTransferi.com</h1>
-    </main>
-  );
-}
