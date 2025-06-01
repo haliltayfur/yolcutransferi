9c25ea6ed05795f725282bd10e7440e57aaa3ea1
haliltayfur <byhaliltayfur@hotmail.com>
2025-06-01 14:55:43 +0300

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,12 +1,14 @@
+// middleware.js
 import createMiddleware from 'next-intl/middleware';
 import { locales, defaultLocale } from './next-intl.config';
 
 export default createMiddleware({
   locales,
   defaultLocale,
-  localeDetection: true,
+  localeDetection: false
 });
 
 export const config = {
-  matcher: ['/((?!api|_next|.*\\..*).*)'],
+  // sadece /api, /_next ve statik dosyalar hariç tutulur
+  matcher: ['/((?!api|_next|.*\\..*).*)']
 };
