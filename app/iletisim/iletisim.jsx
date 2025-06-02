3378e7cfa521949e3f7126d5575e00a4a102f547
haliltayfur <byhaliltayfur@hotmail.com>
2025-06-02 20:19:31 +0300

    Update iletisim.jsx

--- app/iletisim/iletisim.jsx
+++ app/iletisim/iletisim.jsx
@@ -105,9 +105,7 @@ export default function Iletisim() {
         />
       </div>
 
-      <div className="mt-7 text-center text-xs text-gray-400">
-        Tüm verileriniz gizli tutulur, üçüncü kişilerle paylaşılmaz.
-      </div>
+
     </main>
   );
 }
