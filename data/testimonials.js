3ccc8da1a223e7f2132bf8299626e2d21268b5ce
haliltayfur <byhaliltayfur@hotmail.com>
2025-06-10 23:46:38 +0300

    Create testimonials.js

---
+++ data/testimonials.js
@@ -0,0 +1,4 @@
+export const testimonials = [
+  { name: "Deniz Y.", role: "CEO / İstanbul", comment: "Her anlamda profesyonel, gizlilik üst düzey. Misafirlerim için tek adresim.", avatar: "D" },
+  // ... kalan 14 yorumu buradan ekle (eski koddan kopyala)
+];
