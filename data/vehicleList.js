14e902cf3e55e6e9563ebf3c71f87a45add7ceeb
haliltayfur <byhaliltayfur@hotmail.com>
2025-06-11 02:12:07 +0300

    Update vehicleList.js

--- data/vehicleList.js
+++ data/vehicleList.js
@@ -1,3 +1,4 @@
+// /data/vehicles.js
 export const vehicles = [
   { value: "mercedes_vito", label: "Mercedes Vito", max: 7 },
   { value: "s_class", label: "Mercedes S-Class", max: 4 },
@@ -6,5 +7,5 @@ export const vehicles = [
   { value: "premium_sedan", label: "Premium Sedan", max: 4 },
   { value: "cadillac", label: "Cadillac", max: 6 },
   { value: "dron", label: "Dron", max: 2 },
-  { value: "minibus", label: "Minibüs", max: 17 },
+  { value: "minibus", label: "Minibüs", max: 17 }
 ];
