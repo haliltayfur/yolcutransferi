7b773ef24d80f018841367b2f7601fa986892833
haliltayfur <byhaliltayfur@hotmail.com>
2025-06-10 23:45:31 +0300

    Create advantages.js

---
+++ data/advantages.js
@@ -0,0 +1,11 @@
+import { FaLock, FaGlobe, FaIdCard, FaCreditCard, FaUserTie, FaStar, FaPlane, FaBus } from "react-icons/fa";
+export const advantages = [
+  { icon: <FaLock size={22} />, text: "Kişiye özel, tam gizlilikte VIP transfer." },
+  { icon: <FaGlobe size={22} />, text: "Çok dilli, uluslararası eğitimli sürücüler." },
+  { icon: <FaIdCard size={22} />, text: "TÜRSAB belgeli, sigortalı ve güvenli yolculuk." },
+  { icon: <FaCreditCard size={22} />, text: "TROY güvencesiyle %100 yerli ödeme altyapısı." },
+  { icon: <FaUserTie size={22} />, text: "5+ yıllık tecrübeli şoförler ve ayrıcalıklı hizmet." },
+  { icon: <FaStar size={22} />, text: "%99.97 müşteri memnuniyeti ve lüks konfor." },
+  { icon: <FaPlane size={22} />, text: "Havalimanı ve şehirlerarası VIP transfer." },
+  { icon: <FaBus size={22} />, text: "Büyük gruplar için Minibüs ve Dron ile transfer." },
+];
