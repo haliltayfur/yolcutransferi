912688838ecedff53a9330c13bc380d8f046f98a
haliltayfur <byhaliltayfur@hotmail.com>
2025-06-11 02:36:13 +0300

    Update vehicles.js

--- data/vehicles.js
+++ data/vehicles.js
@@ -1,10 +1,11 @@
+// data/vehicles.js
 export const vehicles = [
   { value: "mercedes_vito", label: "Mercedes Vito", max: 7 },
   { value: "s_class", label: "Mercedes S-Class", max: 4 },
-  { value: "vip_minivan", label: "VIP Minivan", max: 8 },
-  { value: "suv", label: "SUV", max: 5 },
+  { value: "vip_minivan", label: "VIP Minivan", max: 6 },
+  { value: "suv", label: "SUV", max: 4 },
   { value: "premium_sedan", label: "Premium Sedan", max: 4 },
-  { value: "cadillac", label: "Cadillac", max: 6 },
+  { value: "cadillac", label: "Cadillac", max: 5 },
   { value: "dron", label: "Dron", max: 2 },
   { value: "minibus", label: "Minibüs", max: 17 },
 ];
