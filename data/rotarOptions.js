4065cecf4b78370e1cc5bf33f38eb76e26ba522b
haliltayfur <byhaliltayfur@hotmail.com>
2025-06-11 02:14:19 +0300

    Update rotarOptions.js

--- data/rotarOptions.js
+++ data/rotarOptions.js
@@ -1,8 +1,9 @@
+// /data/rotarOptions.js veya /data/rotarList.js
 export const rotarList = [
   { hours: 1, label: "1 Saat Ücretsiz", price: 0 },
   { hours: 2, label: "2 Saat (Ekstra 200₺)", price: 200 },
   { hours: 3, label: "3 Saat (Ekstra 300₺)", price: 300 },
   { hours: 4, label: "4 Saat (Ekstra 400₺)", price: 400 },
   { hours: 5, label: "5 Saat (Ekstra 500₺)", price: 500 },
-  { hours: 6, label: "Sınırsız/İptal Olmaz (1000₺)", price: 1000 },
+  { hours: 6, label: "Sınırsız/İptal Olmaz (1000₺)", price: 1000 }
 ];
