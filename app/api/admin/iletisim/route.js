e1cad69630f2799a005f3efcd5a42d7c0d02b6c7
haliltayfur <byhaliltayfur@hotmail.com>
2025-06-13 20:35:21 +0300

    Update route.js

--- app/api/admin/iletisim/route.js
+++ app/api/admin/iletisim/route.js
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import { connectToDatabase } from "@/lib/mongodb";
+import { connectToDatabase } from "../../../../lib/mongodb";
 
 // GET /api/admin/iletisim
 export async function GET() {
