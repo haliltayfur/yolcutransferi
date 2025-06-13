e94e356f51bfe2a3ff36f481ea1bd8ee440de863
haliltayfur <byhaliltayfur@hotmail.com>
2025-06-13 16:24:16 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,20 +2,20 @@
 export default {
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
     extend: {
       colors: {
-        gold: "#d4af37",
-        navy: "#1a2238",
-        gray: "#F5F5F5"
+        gold: '#8c7327'
       },
-      fontFamily: {
-        serif: ["DM Serif Display", "serif"],
-        sans: ["Montserrat", "sans-serif"]
+      boxShadow: {
+        'gold-lg': '0 4px 24px 0 #8c732770',
+        'gold-xl': '0 6px 30px 0 #e7c776a0'
       }
-    },
+    }
   },
-  plugins: [],
+  plugins: []
 }
