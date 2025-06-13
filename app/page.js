b63a8a0ed5095dcc58b71582a0fdde31d9c73ce8
haliltayfur <byhaliltayfur@hotmail.com>
2025-06-13 17:03:11 +0300

    Update page.js

--- app/page.js
+++ app/page.js
@@ -2,7 +2,6 @@ import HeroSlider from "../components/HeroSlider";
 import VipTransferForm from "../components/VipTransferForm";
 import AdvantagesBar from "../components/AdvantagesBar";
 import TestimonialsSlider from "../components/TestimonialsSlider";
-import SectionTitle from "../components/SectionTitle"; // yeni başlık bileşeni
 
 export default function Home() {
   return (
@@ -11,17 +10,18 @@ export default function Home() {
       <VipTransferForm />
 
       {/* Neden YolcuTransferi alanı */}
-  <section className="w-full flex flex-col items-center mt-6 mb-12">
-  <h2 className="text-3xl md:text-4xl font-bold mb-4 text-gold text-center drop-shadow">
-    Neden YolcuTransferi?
-  </h2>
-  <AdvantagesBar />
-</section>
+      <section className="w-full flex flex-col items-center mt-6 mb-12">
+        <h2 className="text-3xl md:text-4xl font-bold mb-4 text-gold text-center drop-shadow">
+          Neden YolcuTransferi?
+        </h2>
+        <AdvantagesBar />
+      </section>
 
       {/* Yorumlar alanı */}
       <section className="w-full flex flex-col items-center mt-6 mb-12">
-          <h2 className="text-3xl md:text-4xl font-bold mb-4 text-gold text-center drop-shadow">
-         Müşteri Deneyimleri & Yorumları
+        <h2 className="text-3xl md:text-4xl font-bold mb-4 text-gold text-center drop-shadow">
+          Müşteri Deneyimleri & Yorumları
+        </h2>
         <TestimonialsSlider />
       </section>
     </main>
