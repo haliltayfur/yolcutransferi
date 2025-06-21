e735887d4f4172f705d3ea1ec7d3dd8937748995
haliltayfur <byhaliltayfur@hotmail.com>
2025-06-22 00:06:41 +0300

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
-import HeroSlider from "../components/HeroSlider";
-import HeroVideo from "../components/HeroVideo";
 import VipTransferForm from "../components/VipTransferForm";
+import HeroVideo from "../components/HeroVideo";
+import HeroSlider from "../components/HeroSlider";
 import AdvantagesBar from "../components/AdvantagesBar";
 import TestimonialsSlider from "../components/TestimonialsSlider";
 
@@ -8,14 +8,9 @@ export default function Home() {
   return (
     <main>
       <HeroSlider />
-      {/* Transfer Planlama + Video Yan Yana */}
-      <section className="flex flex-col md:flex-row justify-center items-start gap-8 px-4 max-w-6xl mx-auto mt-10 mb-12">
-        <div className="flex-1">
-          <VipTransferForm />
-        </div>
-        <div className="w-full md:w-[300px] flex justify-end">
-          <HeroVideo />
-        </div>
+      <section className="flex flex-col md:flex-row justify-start items-start gap-8 px-4 max-w-6xl mx-auto mt-10 mb-12">
+        <VipTransferForm />
+        <HeroVideo />
       </section>
       <AdvantagesBar />
       <TestimonialsSlider />
