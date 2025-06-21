de713afe41b51b85cd529987324ecb34d0b9b4c7
haliltayfur <byhaliltayfur@hotmail.com>
2025-06-21 23:53:57 +0300

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,5 +1,5 @@
 import HeroSlider from "../components/HeroSlider";
-import HeroVideo from "../components/HeroVideo";   // <--- Yeni ekle
+import HeroVideo from "../components/HeroVideo";
 import VipTransferForm from "../components/VipTransferForm";
 import AdvantagesBar from "../components/AdvantagesBar";
 import TestimonialsSlider from "../components/TestimonialsSlider";
@@ -8,8 +8,15 @@ export default function Home() {
   return (
     <main>
       <HeroSlider />
-      <HeroVideo />   {/* Videon burada, ister slider'dan önce ister sonra koyabilirsin */}
-      <VipTransferForm />
+      {/* Transfer Planlama + Video Yan Yana */}
+      <section className="flex flex-col md:flex-row justify-center items-start gap-8 px-4 max-w-6xl mx-auto mt-10 mb-12">
+        <div className="flex-1">
+          <VipTransferForm />
+        </div>
+        <div className="w-full md:w-[300px] flex justify-end">
+          <HeroVideo />
+        </div>
+      </section>
       <AdvantagesBar />
       <TestimonialsSlider />
     </main>
