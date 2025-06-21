b254bbbe2df5743d385a81c30470200c759dcd42
haliltayfur <byhaliltayfur@hotmail.com>
2025-06-22 01:04:50 +0300

    Update page.js

--- app/page.js
+++ app/page.js
@@ -8,9 +8,9 @@ export default function Home() {
   return (
     <main>
       <HeroSlider />
-      <section className="flex flex-col md:flex-row justify-center items-center gap-8 px-4 max-w-[1150px] mx-auto mt-12 mb-14">
+      <section className="flex flex-col md:flex-row justify-center items-center gap-8 px-4 max-w-[1200px] mx-auto mt-12 mb-14">
         {/* Sol blok: Form */}
-        <div className="flex items-center justify-center rounded-2xl border border-gold bg-black/80 shadow-lg w-[750px] h-[600px] min-w-[300px]">
+        <div className="flex items-center justify-center rounded-2xl border border-gold bg-black/80 shadow-lg w-[800px] h-[600px] min-w-[300px]">
           <VipTransferForm />
         </div>
         {/* Sağ blok: Video */}
