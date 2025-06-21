e58947c0c880823b06a8daddd7234357aa74d9fe
haliltayfur <byhaliltayfur@hotmail.com>
2025-06-21 23:49:48 +0300

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,4 +1,5 @@
 import HeroSlider from "../components/HeroSlider";
+import HeroVideo from "../components/HeroVideo";   // <--- Yeni ekle
 import VipTransferForm from "../components/VipTransferForm";
 import AdvantagesBar from "../components/AdvantagesBar";
 import TestimonialsSlider from "../components/TestimonialsSlider";
@@ -7,6 +8,7 @@ export default function Home() {
   return (
     <main>
       <HeroSlider />
+      <HeroVideo />   {/* Videon burada, ister slider'dan önce ister sonra koyabilirsin */}
       <VipTransferForm />
       <AdvantagesBar />
       <TestimonialsSlider />
