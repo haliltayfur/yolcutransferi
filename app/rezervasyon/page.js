d43d86aab7cfd14dca636e2fbea11e30a56d670c
haliltayfur <byhaliltayfur@hotmail.com>
2025-06-25 22:06:15 +0300

    Update page.js

--- app/rezervasyon/page.js
+++ app/rezervasyon/page.js
@@ -3,12 +3,12 @@ import RezervasyonForm from "./RezervasyonForm";
 
 export default function Page() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-gradient-to-br from-black via-[#19160a] to-[#302811]">
-      <div className="w-full max-w-2xl mx-auto rounded-3xl border border-[#bfa658] bg-black/90 shadow-2xl px-8 py-10">
+    <section className="flex justify-center items-center min-h-[92vh] bg-gradient-to-br from-black via-[#19160a] to-[#302811] py-10">
+      <div className="w-full max-w-2xl mx-auto border border-[#bfa658] rounded-3xl shadow-2xl bg-black/90 px-4 md:px-10 py-8 md:py-14">
         <Suspense fallback={<div>Yükleniyor...</div>}>
           <RezervasyonForm />
         </Suspense>
       </div>
-    </div>
+    </section>
   );
 }
