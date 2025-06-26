a1551812a3e0bbdde410b345946de729bc985606
haliltayfur <byhaliltayfur@hotmail.com>
2025-06-27 01:42:03 +0300

    Update page.js

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ import HeroVideo from "../components/HeroVideo";
 import AdvantagesBar from "../components/AdvantagesBar";
 import TestimonialsSlider from "../components/TestimonialsSlider";
 
-// ŞU SATIRDA dynamic import!
+// SSR devre dışı, client-side
 const VipTransferForm = dynamic(() => import("../components/VipTransferForm"), { ssr: false });
 
 export default function Home() {
@@ -13,21 +13,22 @@ export default function Home() {
       <HeroSlider />
       <section
         className="
-          flex flex-col items-center
-          md:flex-row md:justify-center md:items-start
-          gap-0 md:gap-[5px]
+          flex flex-col md:flex-row
+          justify-center items-center
+          gap-8 md:gap-16
+          w-full max-w-[1200px] mx-auto
           px-2 md:px-0
-          max-w-[1200px] mx-auto
           mt-8 md:mt-12
           mb-8 md:mb-14
+          transition-all
         "
       >
         {/* FORM */}
-        <div className="w-full max-w-[340px] md:max-w-[810px] flex items-center justify-center transition-all">
+        <div className="w-full max-w-[410px] flex items-center justify-center">
           <VipTransferForm />
         </div>
         {/* VIDEO sadece desktop */}
-        <div className="hidden md:block w-[380px] h-[600px] flex-shrink-0 overflow-hidden">
+        <div className="hidden md:flex w-[370px] h-[480px] items-center justify-center rounded-2xl overflow-hidden shadow-xl border border-[#bfa658] bg-black/90">
           <HeroVideo />
         </div>
       </section>
