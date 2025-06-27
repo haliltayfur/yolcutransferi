9ecfb90911e9af3feb200b90e4d3417108cfa0fb
haliltayfur <byhaliltayfur@hotmail.com>
2025-06-27 20:37:25 +0300

    Update page.js

--- app/page.js
+++ app/page.js
@@ -6,7 +6,6 @@ import HeroVideo from "../components/HeroVideo";
 import AdvantagesBar from "../components/AdvantagesBar";
 import TestimonialsSlider from "../components/TestimonialsSlider";
 
-// SSR devre dışı, client-side
 const VipTransferForm = dynamic(() => import("../components/VipTransferForm"), { ssr: false });
 
 export default function Home() {
@@ -15,38 +14,50 @@ export default function Home() {
       <HeroSlider />
       <section
         className="
-          flex flex-col md:flex-row
-          justify-center items-start
-          gap-10 md:gap-16
-          w-full max-w-[1200px] mx-auto
+          flex flex-row justify-center items-start
+          gap-[10px]
+          w-full max-w-[1260px] mx-auto
           px-2 md:px-0
           mt-12 md:mt-16
           mb-10 md:mb-16
+          min-h-[600px]
         "
+        style={{
+          minHeight: 600,
+        }}
       >
         {/* FORM */}
         <div
           className="
-            w-full max-w-[440px] min-h-[570px]
             flex flex-col justify-center
             bg-black/85 border border-[#bfa658]
             rounded-2xl shadow-2xl
-            p-7 md:p-8
-            mx-auto
+            p-0
           "
+          style={{
+            width: 800,
+            height: 600,
+            minWidth: 800,
+            minHeight: 600,
+            boxSizing: "border-box",
+          }}
         >
           <VipTransferForm />
         </div>
-        {/* VIDEO sadece desktop */}
+        {/* VIDEO */}
         <div
           className="
-            hidden md:flex
-            items-center justify-center
+            flex items-center justify-center
             rounded-2xl overflow-hidden shadow-2xl border border-[#bfa658]
             bg-black/90
-            ml-2
-            w-[340px] h-[570px] min-w-[260px]
           "
+          style={{
+            width: 400,
+            height: 600,
+            minWidth: 400,
+            minHeight: 600,
+            boxSizing: "border-box",
+          }}
         >
           <HeroVideo />
         </div>
