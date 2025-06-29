24aa4e824b4ced3e6471f2e4c609c0ec5abd1f57
haliltayfur <byhaliltayfur@hotmail.com>
2025-06-29 23:04:13 +0300

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -3,7 +3,7 @@
 import "./globals.css";
 import Header from "../components/Header";
 import Footer from "../components/Footer"; // Footer'ı da ayrı bileşen olarak ekliyoruz
-import { SpeedInsights } from "@vercel/speed-insights/next"; // <-- YENİ EKLENDİ
+
 
 export const metadata = {
   title: "YolcuTransferi.com",
