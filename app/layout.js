ca36855474ad12f72acb3e8109145247a07f4f67
haliltayfur <byhaliltayfur@hotmail.com>
2025-06-29 15:02:17 +0300

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,9 @@
+// app/layout.js veya app/layout.jsx
+
 import "./globals.css";
 import Header from "../components/Header";
 import Footer from "../components/Footer"; // Footer'ı da ayrı bileşen olarak ekliyoruz
+import { SpeedInsights } from "@vercel/speed-insights/next"; // <-- YENİ EKLENDİ
 
 export const metadata = {
   title: "YolcuTransferi.com",
@@ -14,6 +17,7 @@ export default function RootLayout({ children }) {
         <Header />
         <main className="flex-grow">{children}</main>
         <Footer /> {/* VIP zengin Footer sadece burada! */}
+        <SpeedInsights /> {/* <-- EN ALTA BUNU EKLE, Vercel için! */}
       </body>
     </html>
   );
