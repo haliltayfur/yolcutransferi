38719340c9b1e484759f8ee0549eee2f81560c3e
haliltayfur <byhaliltayfur@hotmail.com>
2025-07-02 18:42:14 +0300

    Update page.js

--- app/iletisim/page.js
+++ app/iletisim/page.js
@@ -1,13 +1,8 @@
-
 "use client";
-import React from "react";
-import IletisimForm from "./iletisim";
+import IletisimForm from "./IletisimForm";
 
 export default function Page() {
-  return (
-    <main className="flex justify-center items-center min-h-[90vh] bg-black">
-      <IletisimForm />
-    </main>
-  );
+  return <IletisimForm />;
 }
+
 // app/iletisim/page.js
