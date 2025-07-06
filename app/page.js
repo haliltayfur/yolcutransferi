a3737725e3127e236c971f1afcb663ad3b7c447b
haliltayfur <byhaliltayfur@hotmail.com>
2025-07-06 22:16:28 +0300

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,5 +1,4 @@
 // PATH: app/page.js
-
 "use client";
 import HeroSlider from "../components/HeroSlider";
 import RezervasyonHero from "../components/RezervasyonHero";
@@ -9,16 +8,9 @@ import TestimonialsSlider from "../components/TestimonialsSlider";
 export default function Home() {
   return (
     <main>
-      {/* Hero Slider (sayfanın üstü) */}
       <HeroSlider />
-
-      {/* Rezervasyon Formu + Video */}
       <RezervasyonHero />
-
-      {/* Neden YolcuTransferi (Avantajlar) */}
       <AdvantagesBar />
-
-      {/* Müşteri Yorumları */}
       <TestimonialsSlider />
     </main>
   );
