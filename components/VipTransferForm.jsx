f92d8040c8a18b127fbb2d78bba721210e1a4aad
haliltayfur <byhaliltayfur@hotmail.com>
2025-07-07 22:13:10 +0300

    Update VipTransferForm.jsx

--- components/VipTransferForm.jsx
+++ components/VipTransferForm.jsx
@@ -2,9 +2,9 @@
 
 "use client";
 import React, { useState, useEffect } from "react";
-import EkstralarAccordion from "../../data/EkstralarAccordion";
-import { vehicles } from "../../data/vehicleList";
-import { extrasListByCategory } from "../../data/extrasByCategory";
+import EkstralarAccordion from "../../data/EkstralarAccordion.jsx";
+import { vehicles } from "../../data/vehicleList.js";
+import { extrasListByCategory } from "../../data/extrasByCategory.js";
 import { useRouter } from "next/navigation";
 
 // === Otomatik Adres ===
