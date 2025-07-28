4578f2c0ec528b448ffedbd96b2d30363c2b23b3
haliltayfur <byhaliltayfur@hotmail.com>
2025-07-29 00:08:10 +0300

    Create page.js

---
+++ app/_not-found/page.js
@@ -0,0 +1,9 @@
+// app/_not-found/page.js
+export default function NotFound() {
+  return (
+    <div style={{ padding: 48, textAlign: "center" }}>
+      <h1>404</h1>
+      <p>Sayfa bulunamadı.</p>
+    </div>
+  );
+}
